c8c51d25fdafe7cc2af9d3fca3a44344b6eee2ba
Simon Mittag <simon@sypht.com>
2019-09-14 08:44:48 +1000

    initial revision

---
+++ Sypht.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sypht_csharp_client
+{
+    class Sypht
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
