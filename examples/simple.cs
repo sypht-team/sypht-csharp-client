9a8ccb102017f9686b22681667fa302cf323cc83
Simon Mittag <simon@sypht.com>
2019-09-14 23:17:18 +1000

    refactored

--- examples/simple.cs
+++ examples/simple.cs
@@ -8,8 +8,7 @@ namespace Sypht.Examples
         static async Task Main(string[] args)
         {
             var sypht = new Client();
-            var fileId = await sypht.upload("../../../examples/receipt.pdf");
-            Console.WriteLine(await sypht.result(fileId));
+            Console.WriteLine(await sypht.result(await sypht.upload("../../../examples/receipt.pdf")));
         }
     }
 }
